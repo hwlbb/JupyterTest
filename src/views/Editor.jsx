fee5e9879d2c3ca1a69ce12f67afc47b5929b546
qpdbpp@qq.com <qpdbpp@qq.com>
2024-06-03 16:44:14 +0800

    pbvr url

--- src/views/Editor.jsx
+++ src/views/Editor.jsx
@@ -1,7 +1,7 @@
 export default function Editor() {
   return (
     <iframe
-      src="http://192.168.1.120:8888/lab?token=this_is_a_token"
+      src="http://jupyterlab.pbvr.org.cn/?token=a_token"
       className="iframe-content"
       title="Content"
       style={{ width: "100%", height: "100%", border: "none" }}
